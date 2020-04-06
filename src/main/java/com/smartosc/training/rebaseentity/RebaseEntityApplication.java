6832dedcfc6bca491befe7cea3b6fcc63eeb9b0d
Andy239 <duongan98w@gmail.com>
2020-04-06 22:29:49 +0700

    initial commit

---
+++ src/main/java/com/smartosc/training/rebaseentity/RebaseEntityApplication.java
@@ -0,0 +1,13 @@
+package com.smartosc.training.rebaseentity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebaseEntityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RebaseEntityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartosc/training/rebaseentity/RebaseEntityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartosc.training.rebaseentity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RebaseEntityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
